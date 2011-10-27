f110fafc7e12312e14c3db5026384b321ea1ee89
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2011-10-27 21:08:08 +0000

    Added/updated some translation files.

--- trans/cda-config-editor_hu.ts
+++ trans/cda-config-editor_hu.ts
@@ -5,7 +5,7 @@
     <name>ConfigEditorWindow</name>
     <message>
         <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="53"/>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="110"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="96"/>
         <source>CDA configuration editor - Untitled.cxml</source>
         <translation type="unfinished"></translation>
     </message>
@@ -15,194 +15,237 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="118"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="104"/>
         <source>Open setup file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="119"/>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="161"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="105"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="145"/>
         <source>default.cxml</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="120"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="106"/>
         <source>CDA XML setup files (*.cxml);;CDA binary setup files (*.cbin);;All files (*)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="160"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="144"/>
         <source>Save setup file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="162"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="146"/>
         <source>CDA XML setup files (*.cxml);;CDA binary setup files (*.cbin)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="184"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="167"/>
         <source>CDA Configuration Editor - built on Qt</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="198"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="179"/>
         <source>CDA Configuration Editor</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="199"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="180"/>
         <source>This application can be used to construct configuration files for the CDA applications. The files can be read and saved in both binary and XML format.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="215"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="195"/>
         <source>&amp;File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="218"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="198"/>
         <source>&amp;Create new configuration...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="223"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="203"/>
         <source>&amp;Open configuration file...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="228"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="208"/>
         <source>&amp;Save configuration file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="233"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="213"/>
         <source>Save configuration as...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="240"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="220"/>
         <source>&amp;Quit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="251"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="231"/>
         <source>&amp;Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="253"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="233"/>
         <source>About Qt</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="257"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="237"/>
         <source>About Config Editor</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="262"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="242"/>
         <source>About CDA</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="272"/>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="365"/>
-        <source>Configuration reading error</source>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="257"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="260"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="325"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="328"/>
+        <source>The specified configuration file (&quot;%1&quot;) could not be opened!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="273"/>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="366"/>
-        <source>The configuration could not be read. See the application messages for more information</source>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="264"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="332"/>
+        <source>Opened file: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="277"/>
+        <source>Error in parsing &quot;%1&quot;
+  Error message: %2
+  Error line   : %3
+  Error column : %4</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="284"/>
-        <source>The specified configuration file (&quot;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="287"/>
-        <source>File not opened</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="288"/>
-        <source>The specified XML file (&quot;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="289"/>
-        <source>&quot;) could not be opened!</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="308"/>
-        <source>XML error</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="309"/>
         <source>There is some problem with the format of the input file.
 
- </source>
+ Error message: %1
+Error line: %2
+Error column: %3</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="311"/>
-        <source>Error message: </source>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="292"/>
+        <source>Successfully parsed: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="303"/>
+        <source>Failed to read configuration file!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="312"/>
-        <source>Error line: </source>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="358"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="418"/>
+        <source>CDA configuration editor - %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="313"/>
-        <source>Error column: </source>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="341"/>
+        <source>Some error happened while reading the binary configuration</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="335"/>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="378"/>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="434"/>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="477"/>
-        <source>CDA configuration editor - </source>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="344"/>
+        <source>Configuration reading error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="345"/>
+        <source>The configuration could not be read. See the application messages for more information</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="350"/>
+        <source>Configuration read in binary format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="379"/>
+        <source>Some error happened while creating the XML configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="407"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="440"/>
+        <source>%1 opened for writing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="449"/>
+        <source>Some error happened while creating the binary configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="458"/>
+        <source>Configuration written in binary format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="259"/>
+        <source>File not opened</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="283"/>
+        <source>XML error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="466"/>
+        <source>CDA configuration editor - </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="327"/>
         <source>File reading error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="351"/>
-        <source>The selected file could not be opened for reading. Check the file name!</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="402"/>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="464"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="382"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="452"/>
         <source>Configuration writing error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="403"/>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="465"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="383"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="453"/>
         <source>The configuration could not be written. See the application messages for more information.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="418"/>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="449"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="388"/>
+        <source>Configuration translated to XML format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="398"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="431"/>
+        <source>%1 couldn&apos;t be opened for writing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="401"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="434"/>
         <source>File writing error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="419"/>
-        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="450"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="402"/>
+        <location filename="../apps/cda-config-editor/ConfigEditorWindow.cxx" line="435"/>
         <source>The selected file could not be opened for writing. Check permissions!</source>
         <translation type="unfinished"></translation>
     </message>
---
+++ trans/cda-config-server_hu.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hu_HU">
+<context>
+    <name>cda-config-server</name>
+    <message>
+        <location filename="../apps/cda-config-server/main.cxx" line="77"/>
+        <source>Didn&apos;t recognise verbosity level setting
+Terminating...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-server/main.cxx" line="90"/>
+        <source>The configuration server could not be started on address: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../apps/cda-config-server/main.cxx" line="97"/>
+        <source>The configuration server is broadcasting &quot;%1&quot; on address &quot;%2&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
