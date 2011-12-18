29d3430e85e907d1ac5bbc81491f4a54c057bcd7
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2011-12-18 19:31:23 +0000

    Making CDA work on Windows once more

--- apps/plugin_app.pri
+++ apps/plugin_app.pri
@@ -10,6 +10,12 @@
 
 # Link the application against all available plugins on Windows:
 win32 {
+   # Statically link Windows applications against the plugins:
    LIBS += -L../../dev -lad413a -lad1000 -lad2249a \
            -lt2228a -lt4300b -ls9418t -ldt5740
+
+   # Define the plugin resources explicitly for the Windows
+   # applications, because they are not loaded properly from
+   # the plugins on Windows for some reason...
+   RESOURCES   += ../../devices/dt5740/dt5740.qrc
 }
--- gui/common/DefaultFont.h
+++ gui/common/DefaultFont.h
@@ -5,6 +5,7 @@
 
 // Qt include(s):
 #include <QtGui/QFont>
+#include <QtGui/QApplication>
 
 namespace gui {
 
@@ -24,7 +25,16 @@ namespace gui {
 
    public:
       /// Constructor setting all default properties
-      DefaultFont();
+      DefaultFont()
+#ifdef Q_OS_DARWIN
+         // Don't modify the application default on Mac OS X:
+         : QFont( QApplication::font() ) {
+#else
+         // On other platforms use a 10pt Helvetica font:
+         : QFont( "Helvetica [Cronyx]", 10 ) {
+#endif // Q_OS_DARWIN
+
+      }
 
    }; // class DefaultFont
 
