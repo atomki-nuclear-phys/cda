b0a3fb80713fef174c2bb4f2a00756b3bc3470e7
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2016-03-29 18:49:30 +0000

    Starting to implement some functionality into caen::VmeDevice. Very bare bones so far...

--- core/caen/VmeDevice.cxx
+++ core/caen/VmeDevice.cxx
@@ -1,17 +1,121 @@
 // $Id$
 
+// System include(s):
+#include <cstring>
+
+// CAEN include(s):
+#ifdef HAVE_CAEN_QTP_LIBS
+#   include <cvt_board_commons.h>
+#   include <cvt_common_defs.h>
+#   include <cvt_V792.h>
+#endif
+
+// CDA include(s):
+#include "../msg/Logger.h"
+#include "../common/errorcheck.h"
+
 // Local include(s):
 #include "VmeDevice.h"
 #include "VmeBus.h"
 
+namespace {
+
+#ifdef HAVE_CAEN_QTP_LIBS
+   /// Helper function converting between device types
+   CVT_QTP_TYPES convert( caen::VmeDevice::DeviceType type ) {
+      switch( type ) {
+         case caen::VmeDevice::DEV_V775A:
+            return CVT_V775_TYPE_A;
+            break;
+         case caen::VmeDevice::DEV_V785A:
+            return CVT_V785_TYPE_A;
+            break;
+         case caen::VmeDevice::DEV_V792A:
+            return CVT_V792_TYPE_A;
+            break;
+         case caen::VmeDevice::DEV_V862A:
+            return CVT_V862_TYPE_A;
+            break;
+         default:
+            msg::Logger m_logger( "convert" );
+            REPORT_ERROR( "Unknown device type received: " << type );
+            return static_cast< CVT_QTP_TYPES >( 0 );
+      }
+   }
+#endif // HAVE_CAEN_QTP_LIBS
+
+   /// Helper function for printing pretty messages
+   const char* toString( caen::VmeDevice::DeviceType type ) {
+      switch( type ){
+         case caen::VmeDevice::DEV_V775A:
+            return "V775A";
+            break;
+         case caen::VmeDevice::DEV_V785A:
+            return "V785A";
+            break;
+         case caen::VmeDevice::DEV_V792A:
+            return "V792A";
+            break;
+         case caen::VmeDevice::DEV_V862A:
+            return "V862A";
+            break;
+         default:
+            return "<unknown>";
+      }
+   }
+
+} // private namespace
+
 namespace caen {
 
-   VmeDevice::VmeDevice() {
+   /// Helper class holding on to data used by the QTP library
+   class VmeDevicePrivateData {
 
+   public:
+      /// Default constructor
+      VmeDevicePrivateData() {
+#ifdef HAVE_CAEN_QTP_LIBS
+         // Make sure that the data object is zeroed out at the start
+         memset( &m_data, 0, sizeof( m_data ) );
+#endif // HAVE_CAEN_QTP_LIBS
+      }
+
+#ifdef HAVE_CAEN_QTP_LIBS
+      /// Accessor for the data object
+      cvt_V792_data* data() { return &m_data; }
+
+   private:
+      /// Data object used by the QTP library
+      cvt_V792_data m_data;
+#endif // HAVE_CAEN_QTP_LIBS
+
+   }; // class VmeDevicePrivateData
+
+   VmeDevice::VmeDevice()
+      : m_data( 0 ), m_logger( "caen::VmeDevice" ) {
+
+   }
+
+   VmeDevice::~VmeDevice() {
+
+      if( m_data ) {
+         delete m_data;
+      }
    }
 
    bool VmeDevice::open( uint16_t address, DeviceType type,
                          const VmeBus& bus ) {
+
+      // Make sure that the data is cleared:
+      resetData();
+
+      // Open the connection to the device:
+#ifdef HAVE_CAEN_QTP_LIBS
+      CHECK( cvt_V792_open( m_data->data(), address, bus.handle(),
+                            ::convert( type ) ) );
+#endif // HAVE_CAEN_QTP_LIBS
+      REPORT_VERBOSE( tr( "Device \"%1\" opened on address %2" )
+                      .arg( toString( type ) ).arg( address, 4, 16 ) );
 
       // Return gracefully:
       return true;
@@ -19,8 +123,32 @@ namespace caen {
 
    bool VmeDevice::close() {
 
+      // Make sure that we have a data object:
+      if( ! m_data ) {
+         m_logger << msg::WARNING << tr( "Trying to close a closed device" )
+                  << msg::endmsg;
+         return false;
+      }
+
+      // Close the connection to the device:
+#ifdef HAVE_CAEN_QTP_LIBS
+      CHECK( cvt_V792_close( m_data->data() ) );
+#endif // HAVE_CAEN_QTP_LIBS
+      REPORT_VERBOSE( tr( "Device closed" ) );
+
       // Return gracefully:
       return true;
+   }
+
+   void VmeDevice::resetData() {
+
+      if( m_data ) {
+         delete m_data;
+         m_data = 0;
+      }
+      m_data = new VmeDevicePrivateData();
+
+      return;
    }
 
 } // namespace caen
--- core/caen/VmeDevice.h
+++ core/caen/VmeDevice.h
@@ -6,13 +6,18 @@
 // System include(s):
 #include <cinttypes>
 
+// Qt include(s):
+#include <QtCore/QCoreApplication>
+
 // Local include(s):
 #include "../common/Export.h"
+#include "../msg/Logger.h"
 
 namespace caen {
 
    // Forward declaration(s):
    class VmeBus;
+   class VmeDevicePrivateData;
 
    /**
     * @brief Interface to the CAENqtpSDK library
@@ -30,21 +35,37 @@ namespace caen {
     */
    class CDACORE_EXPORT VmeDevice {
 
+      // To get the tr() function:
+      Q_DECLARE_TR_FUNCTIONS( caen::VmeDevice )
+
    public:
       /// Default constructor
       VmeDevice();
+      /// Destructor
+      ~VmeDevice();
 
       /// Supported device types
       enum DeviceType {
          DEV_V775A = 0, ///< The board is a V775A
          DEV_V785A = 1, ///< The board is a V785A
-         DEV_V792A = 2  ///< The board is a V792A
+         DEV_V792A = 2, ///< The board is a V792A
+         DEV_V862A = 3  ///< The board is a V862A
       };
 
       /// Open connection to a CAEN VME device
       bool open( uint16_t address, DeviceType type, const VmeBus& bus );
       /// Close the connection to the device
       bool close();
+
+   private:
+      /// Function making sure that the data object is pristene clear
+      void resetData();
+
+      /// Internal data for the device
+      VmeDevicePrivateData* m_data;
+
+      /// The message logger object
+      msg::Logger m_logger;
 
    }; // class VmeDevice
 
