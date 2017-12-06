64c3504c24ab7c31dbf2f56454b594ebcc3327e8
Attila Krasznahorkay <attila.krasznahorkay@cern.ch>
2017-12-06 18:05:56 +0100

    Added a very simple way in which the executable can now listen to incoming messages. Will have to come up with some better overall organisation in the end though. (Probably won't be able to avoid setting up a separate thread that deals with displaying the messages...)

--- apps/cda-caen-vme-terminal/main.cxx
+++ apps/cda-caen-vme-terminal/main.cxx
@@ -6,11 +6,23 @@
 // Curses include(s):
 #include <ncurses.h>
 
+// Qt include(s):
+#include <QCoreApplication>
+#include <QObject>
+
 // CDA include(s):
+#include "msg/Server.h"
 #include "msg/Message.h"
 #include "msg/TermWindowView.h"
 
-int main() {
+int main( int argc, char* argv[] ) {
+
+   // Set up a Qt console application:
+   QCoreApplication app( argc, argv );
+
+   // Start the message server:
+   msg::Server server;
+   server.listen( Address( "0.0.0.0", 49700 ) );
 
    ::initscr();
    ::cbreak();
@@ -20,20 +32,10 @@ int main() {
 
    {
       msg::TermWindowView messages( 0, 0, COLS, LINES );
-      for( int i = 0; i < 50; ++i ) {
-         std::ostringstream msgNumber;
-         msgNumber << i + 1 << " 12341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234";
-         messages.addMessage( msg::Message( "test", ( "Test INFO message " +
-                                                      msgNumber.str() ).c_str(),
-                                            msg::INFO ) );
-         messages.addMessage( msg::Message( "test", ( "Test WARNING message " +
-                                                      msgNumber.str() ).c_str(),
-                                            msg::WARNING ) );
-      }
-      int ch = 0;
-      while( ( ch = ::getch() ) == KEY_RESIZE ) {
-         messages.resize( 0, 0, COLS, LINES );
-      }
+      QObject::connect( &server,
+                        SIGNAL( messageAvailable( const Message& ) ),
+                        &messages, SLOT( addMessage( const Message& ) ) );
+      app.exec();
    }
 
    ::endwin();
