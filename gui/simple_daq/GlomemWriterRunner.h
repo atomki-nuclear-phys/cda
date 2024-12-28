9a3a57fa12cc8891b71ca2eb48842c68975f4530
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2024-12-29 00:00:15 +0100

    Created a resizable layout for simple_daq::GlomemWriterRunner.

--- gui/simple_daq/GlomemWriterRunner.h
+++ gui/simple_daq/GlomemWriterRunner.h
@@ -1,32 +1,24 @@
-// Dear emacs, this is -*- c++ -*-
+//
+// ATOMKI Common Data Acquisition
+//
+// (c) 2008-2024 ATOMKI, Debrecen, Hungary
+//
+// Apache License Version 2.0
+//
 #ifndef CDA_GUI_SIMPLE_DAQ_GLOMEMWRITERRUNNER_H
 #define CDA_GUI_SIMPLE_DAQ_GLOMEMWRITERRUNNER_H
 
-// STL include(s):
-#include <set>
+// CDA include(s).
+#include "msg/Level.h"
 
-// Qt include(s):
+// Qt include(s).
 #include <QString>
 #include <QWidget>
 
-// CDA include(s):
-#include "common/AppRunner.h"
-#include "msg/Logger.h"
-
-// Local include(s):
-#include "../common/Export.h"
-
-// Forward declaration(s):
-QT_FORWARD_DECLARE_CLASS(QGroupBox)
-QT_FORWARD_DECLARE_CLASS(QPushButton)
-QT_FORWARD_DECLARE_CLASS(QLabel)
+// System include(s).
+#include <memory>
 
 namespace simple_daq {
-
-// Make sure that the Qt classes are available in this namespace:
-using QT_PREPEND_NAMESPACE(QGroupBox);
-using QT_PREPEND_NAMESPACE(QPushButton);
-using QT_PREPEND_NAMESPACE(QLabel);
 
 /**
  *  @short Widget for starting cda-glomem-writer on the local machine
@@ -38,13 +30,13 @@ using QT_PREPEND_NAMESPACE(QLabel);
  *
  * @author Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
  */
-class CDAGUI_EXPORT GlomemWriterRunner : public QWidget {
+class GlomemWriterRunner : public QWidget {
 
    Q_OBJECT
 
 public:
    /// Standard QWidget-type constructor
-   GlomemWriterRunner(QWidget* parent = 0,
+   GlomemWriterRunner(QWidget* parent = nullptr,
                       Qt::WindowFlags flags = Qt::WindowFlags());
    /// Destructor
    ~GlomemWriterRunner();
@@ -87,19 +79,10 @@ private slots:
    void startApp(bool start);
 
 private:
-   QGroupBox* m_mainBox;          ///< Main box holding graphcal objects
-   QPushButton* m_starterButton;  ///< Button starting the application
-   QLabel* m_processStatus;       ///< Label giving feedback of the status
-
-   QString m_configFileName;    ///< Name of the configuration file
-   QString m_msgServerAddress;  ///< Address of the message server(s)
-   QString m_eventAddress;      ///< Address of cda-glomem-writer
-   /// Address(es) of the statistics server(s)
-   std::set<QString> m_statServerAddresses;
-   msg::Level m_level;  ///< Output level of cda-glomem-writer
-
-   mutable daq::AppRunner m_runner;  ///< The object starting cda-glomem-writer
-   mutable msg::Logger m_logger;     ///< Internal logger object
+   /// Internal data type
+   struct Impl;
+   /// Internal data
+   std::unique_ptr<Impl> m_impl;
 
 };  // class GlomemWriterRunner
 
