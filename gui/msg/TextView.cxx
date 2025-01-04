e57cf68de5f5678de4324ae3fdf6db7276ed1222
Attila Krasznahorkay <30694331+krasznaa@users.noreply.github.com>
2025-01-04 14:14:02 +0100

    Modernized msg::TextView. (#6)
    Apart from making it use PIMPL, it now behaves resonably on Windows
    in dark mode.

--- gui/msg/TextView.cxx
+++ gui/msg/TextView.cxx
@@ -1,65 +1,93 @@
+//
+// ATOMKI Common Data Acquisition
+//
+// (c) 2008-2025 ATOMKI, Debrecen, Hungary
+//
+// Apache License Version 2.0
+//
 
-// Qt include(s):
-#include <QStackedLayout>
-#include <QTextDocument>
-#include <QTextEdit>
-#include <QtCore/QString>
+// Local include(s):
+#include "TextView.h"
 
 // CDA include(s):
 #include "msg/Message.h"
 #include "msg/TextStream.h"
 
-// Local include(s):
-#include "TextView.h"
+// Qt include(s):
+#include <QStackedLayout>
+#include <QString>
+#include <QTextDocument>
+#include <QTextEdit>
+
+// System include(s).
+#include <utility>
+
+namespace {
+
+/// Decide whether a dark theme is active
+bool shouldApplyDarkTheme() {
+   const QPalette defaultPalette;
+   return defaultPalette.color(QPalette::WindowText).lightness() >
+          defaultPalette.color(QPalette::Window).lightness();
+}
+
+}  // namespace
 
 namespace msg {
+
+struct TextView::Impl {
+
+   /// Constructor
+   Impl(QWidget& parent) : m_layout{&parent}, m_edit{&parent} {
+
+      m_layout.addWidget(&m_edit);
+   }
+
+   /// The layout used by the widget
+   QStackedLayout m_layout;
+   /// The text field showing the messages
+   QTextEdit m_edit;
+
+   /// Map to set the foreground and background color of the messages
+   std::map<Level, std::pair<QColor, QColor> > m_levelColor;
+
+   /// Level under which messages are not shown
+   Level m_minShownLevel;
+};
 
 /**
  * The constructor creates the QTextEdit object used to present the
  * messages and sets it up to show the messages in a reasonable way.
  */
 TextView::TextView(QWidget* parent, Qt::WindowFlags flags)
-    : QWidget(parent, flags), m_minShownLevel(VERBOSE) {
+    : QWidget(parent, flags), m_impl{new Impl{*this}} {
 
-   // Init the internal maps:
-   initMaps();
+   // Set up the text view widget.
+   m_impl->m_edit.setReadOnly(true);
+   m_impl->m_edit.setFontFamily("courier");
+   m_impl->m_edit.setLineWrapMode(QTextEdit::NoWrap);
+   m_impl->m_edit.document()->setMaximumBlockCount(100);
 
-   // We don't really need a layout, it's just here to make sure the
-   // text edit widget takes up all available space.
-   m_layout = new QStackedLayout(this);
-
-   // Create the main widget. It is configured to use fixed-width fonts
-   // and to only show a maximum of a 100 messages at a time. (The latter
-   // can be changed by the code using the class.)
-   m_edit = new QTextEdit();
-   m_edit->setMinimumSize(450, 100);
-   m_edit->setMaximumSize(20000, 20000);
-   m_edit->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-   m_edit->setReadOnly(true);
-   m_edit->setFontFamily("courier");
-#ifdef Q_OS_DARWIN
-   //      m_edit->setFontPointSize( 12 );
-#else
-   m_edit->setFontPointSize(10);
-#endif  // Q_OS_MAC
-   m_edit->setLineWrapMode(QTextEdit::NoWrap);
-   m_edit->document()->setMaximumBlockCount(100);
-
-   m_layout->addWidget(m_edit);
+   // Set the colour of the different messages.
+   m_impl->m_levelColor[VERBOSE] = {QColor(Qt::cyan), QColor(Qt::black)};
+   m_impl->m_levelColor[DEBUG] = {QColor(Qt::blue), QColor(Qt::white)};
+   m_impl->m_levelColor[INFO] = {
+       m_impl->m_edit.textBackgroundColor(),
+       shouldApplyDarkTheme() ? QColor(Qt::white) : QColor(Qt::black)};
+   m_impl->m_levelColor[WARNING] = {QColor(Qt::yellow), QColor(Qt::black)};
+   m_impl->m_levelColor[ERROR] = {QColor(Qt::red), QColor(Qt::white)};
+   m_impl->m_levelColor[FATAL] = {QColor(Qt::darkRed), QColor(Qt::white)};
+   m_impl->m_levelColor[ALWAYS] = {QColor(Qt::lightGray), QColor(Qt::black)};
 }
 
-TextView::~TextView() {
-
-   delete m_edit;
-   delete m_layout;
-}
+TextView::~TextView() {}
 
 /**
  * @returns The maximum number of messages that can be shown
  */
 int TextView::getMaximumMessageCount() const {
 
-   return m_edit->document()->maximumBlockCount();
+   return m_impl->m_edit.document()->maximumBlockCount();
 }
 
 /**
@@ -67,7 +95,7 @@ int TextView::getMaximumMessageCount() const {
  */
 Level TextView::getMinimumShownLevel() const {
 
-   return m_minShownLevel;
+   return m_impl->m_minShownLevel;
 }
 
 /**
@@ -82,8 +110,9 @@ void TextView::addMessage(const Message& message) {
    //
    // Return right away if the message should not be shown:
    //
-   if (message.getLevel() < m_minShownLevel)
+   if (message.getLevel() < m_impl->m_minShownLevel) {
       return;
+   }
 
    //
    // Format the message into a QString:
@@ -93,67 +122,28 @@ void TextView::addMessage(const Message& message) {
    stream << message;
 
    //
-   // Get the foreground for the message level:
+   // Get the foreground/background color for the message level.
    //
-   std::map<Level, QColor>::const_iterator foreground_it;
-   if ((foreground_it = m_levelToForeground.find(message.getLevel())) ==
-       m_levelToForeground.end()) {
+   auto color_it = m_impl->m_levelColor.find(message.getLevel());
+   if (color_it == m_impl->m_levelColor.end()) {
       // There is something wrong with the level setting!
-      foreground_it = m_levelToForeground.find(INFO);
+      color_it = m_impl->m_levelColor.find(INFO);
    }
 
-   //
-   // Get the background for the message level:
-   //
-   std::map<Level, QColor>::const_iterator background_it;
-   if ((background_it = m_levelToBackground.find(message.getLevel())) ==
-       m_levelToBackground.end()) {
-      // There is something wrong with the level setting!
-      background_it = m_levelToBackground.find(INFO);
-   }
-
-   m_edit->setTextColor(foreground_it->second);
-   m_edit->setTextBackgroundColor(background_it->second);
-   m_edit->append(msg_text);
-
-   return;
+   // Add the message.
+   m_impl->m_edit.setTextBackgroundColor(color_it->second.first);
+   m_impl->m_edit.setTextColor(color_it->second.second);
+   m_impl->m_edit.append(msg_text);
 }
 
 void TextView::setMaximumMessageCount(int count) {
 
-   m_edit->document()->setMaximumBlockCount(count);
-   return;
+   m_impl->m_edit.document()->setMaximumBlockCount(count);
 }
 
 void TextView::setMinimumShownLevel(Level level) {
 
-   m_minShownLevel = level;
-   return;
-}
-
-/**
- * The class uses internal maps to color-code the messages based on
- * their message level. These maps are configured in this function.
- */
-void TextView::initMaps() {
-
-   m_levelToBackground[VERBOSE] = QColor(Qt::cyan);
-   m_levelToBackground[DEBUG] = QColor(Qt::blue);
-   m_levelToBackground[INFO] = QColor(Qt::white);
-   m_levelToBackground[WARNING] = QColor(Qt::yellow);
-   m_levelToBackground[ERROR] = QColor(Qt::red);
-   m_levelToBackground[FATAL] = QColor(Qt::darkRed);
-   m_levelToBackground[ALWAYS] = QColor(Qt::lightGray);
-
-   m_levelToForeground[VERBOSE] = QColor(Qt::black);
-   m_levelToForeground[DEBUG] = QColor(Qt::white);
-   m_levelToForeground[INFO] = QColor(Qt::black);
-   m_levelToForeground[WARNING] = QColor(Qt::black);
-   m_levelToForeground[ERROR] = QColor(Qt::white);
-   m_levelToForeground[FATAL] = QColor(Qt::white);
-   m_levelToForeground[ALWAYS] = QColor(Qt::black);
-
-   return;
+   m_impl->m_minShownLevel = level;
 }
 
 }  // namespace msg
